e90ab2b0319b2681d3d8f54b911323c9ab130718
vibho <1vibho3@gmail.com>
2025-04-23 20:11:43 -0500

    phase 3 implemented go routines and waitgroup for concurrency

--- start.go
+++ start.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 type Store struct {
 	store map[string]string
+	mu sync.RWMutex
 }
 
 func NewStore() *Store{
@@ -13,20 +18,70 @@ func NewStore() *Store{
 }
 
 func (st *Store) setStore(key string, value string){
-	st.store[key] = value
+	st.mu.Lock()
+	defer st.mu.Unlock()
+	st.store[key] = value	
 }
 
 func (st *Store) getStore(key string) string{
+	st.mu.RLock()
+	defer st.mu.RUnlock()
 	return st.store[key]
 }
 
 func main() {
-	instance := NewStore()
-	instance.setStore("apple", "red")
-	instance.setStore("mango", "yellow")
-	color1 := instance.getStore("apple")
-	color2 := instance.getStore("mango")
+	var wg sync.WaitGroup
+	wg.Add(6)
 
-	fmt.Println(color1)
-	fmt.Println(color2)
+	instance := NewStore()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(100 * time.Millisecond)
+		instance.setStore("apple", "red")
+		fmt.Println("Wrote: red")
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println(instance.getStore("apple"))
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(300 * time.Millisecond)
+		instance.setStore("apple", "dark red")
+		fmt.Println("Wrote: dark red")
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(400 * time.Millisecond)
+		instance.setStore("mango", "yellow")
+		fmt.Println("Wrote: Yellow")
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(instance.getStore("mango"))
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(600 * time.Millisecond)
+		fmt.Println(instance.getStore("apple"))
+	}()
+	
+	wg.Wait()
+
+	// color1 :=  instance.getStore("apple")
+	// color2 :=  instance.getStore("mango")
+	
+	
+	// fmt.Println(color1)
+	// fmt.Println(color2)
+
+	
 }
